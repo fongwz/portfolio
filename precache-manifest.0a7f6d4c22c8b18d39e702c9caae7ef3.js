5aa24d2834c2b70a32c1481528e09a702c1c4cad
fongwz <e0035221@u.nus.edu>
2018-11-02 00:51:16 +0800

    Updates

--- precache-manifest.0a7f6d4c22c8b18d39e702c9caae7ef3.js
+++ precache-manifest.0a7f6d4c22c8b18d39e702c9caae7ef3.js
@@ -4,8 +4,8 @@ self.__precacheManifest = [
     "url": "/portfolio/static/media/icons.faff9214.woff"
   },
   {
-    "revision": "d4810b8c7519ca723188",
-    "url": "/portfolio/static/css/main.b4c87793.chunk.css"
+    "revision": "60920e510ba3e61853c2",
+    "url": "/portfolio/static/css/main.724ed25d.chunk.css"
   },
   {
     "revision": "a1a749e89f578a49306ec2b055c073da",
@@ -20,20 +20,20 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
   },
   {
-    "revision": "cd6c777f1945164224dee082abaea03a",
-    "url": "/portfolio/static/media/outline-icons.cd6c777f.woff2"
-  },
-  {
     "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
     "url": "/portfolio/static/media/outline-icons.ef60a4f6.woff"
   },
   {
-    "revision": "9c74e172f87984c48ddf5c8108cabe67",
-    "url": "/portfolio/static/media/flags.9c74e172.png"
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/portfolio/static/media/outline-icons.cd6c777f.woff2"
   },
   {
     "revision": "701ae6abd4719e9c2ada3535a497b341",
     "url": "/portfolio/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/portfolio/static/media/flags.9c74e172.png"
   },
   {
     "revision": "ad97afd3337e8cda302d10ff5a4026b8",
@@ -44,8 +44,8 @@ self.__precacheManifest = [
     "url": "/portfolio/static/media/icons.0ab54153.woff2"
   },
   {
-    "revision": "d4810b8c7519ca723188",
-    "url": "/portfolio/static/js/main.d4810b8c.chunk.js"
+    "revision": "60920e510ba3e61853c2",
+    "url": "/portfolio/static/js/main.60920e51.chunk.js"
   },
   {
     "revision": "e8c322de9658cbeb8a774b6624167c2c",
@@ -60,20 +60,20 @@ self.__precacheManifest = [
     "url": "/portfolio/static/media/icons.b87b9ba5.ttf"
   },
   {
-    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
-    "url": "/portfolio/static/media/icons.8e3c7f55.eot"
-  },
-  {
     "revision": "13db00b7a34fee4d819ab7f9838cc428",
     "url": "/portfolio/static/media/brand-icons.13db00b7.eot"
   },
   {
-    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
-    "url": "/portfolio/static/media/outline-icons.82f60bd0.svg"
-  },
-  {
     "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
     "url": "/portfolio/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/portfolio/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/portfolio/static/media/outline-icons.82f60bd0.svg"
   },
   {
     "revision": "962a1bf31c081691065fe333d9fa8105",
@@ -84,7 +84,7 @@ self.__precacheManifest = [
     "url": "/portfolio/static/css/1.cb318d68.chunk.css"
   },
   {
-    "revision": "c882c199270cd91af70e67fad3829334",
+    "revision": "dbb8b25e99ee38d9af99fbf051990a6b",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.9968387b4f5552fa7f0939333e1f6743.js"
+  "/portfolio/precache-manifest.0a7f6d4c22c8b18d39e702c9caae7ef3.js"
 );
 
 workbox.clientsClaim();
